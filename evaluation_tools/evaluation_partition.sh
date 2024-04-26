92e76801adb3c8b8b4af36a51fcfce33af930460
liuzeli <liuzeli0708@gmail.com>
2024-04-26 02:03:27 -0700

    vit Large noClamp

--- evaluation_tools/evaluation_partition.sh
+++ evaluation_tools/evaluation_partition.sh
@@ -4,9 +4,9 @@ generatePartition(){
 	add=1
 	val=`expr $1 + $add`
 	# ViT Base [1,48]
-	echo "1,$1,$val,48"
+	# echo "1,$1,$val,48"
 	# ViT Large [1,96]
-	# echo "1,$1,$val,96"
+	echo "1,$1,$val,96"
 }
 
 # sbatch upload_eval.job 1,47,48,96 0
@@ -14,9 +14,9 @@ generatePartition(){
 for bit in 2 4 6 8 16
 do
 	# ViT Base [1,47]
-	for pt in `seq 1 47`
+	# for pt in `seq 1 47`
 	# ViT Large [1,95]
-	# for pt in `seq 1 95`
+	for pt in `seq 1 95`
 	do
 		pt=$(generatePartition $pt)
 		sbatch upload_eval.job $pt $bit
